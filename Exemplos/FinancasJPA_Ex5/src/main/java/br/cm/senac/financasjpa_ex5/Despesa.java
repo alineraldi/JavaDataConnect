12ec5bb66ec8e58bb2df3ce9a256bd9bc541155b
Aline <alineraldif@gmail.com>
2025-04-18 16:12:24 -0300

    feat: CRUD com JPA

---
+++ Exemplos/FinancasJPA_Ex5/src/main/java/br/cm/senac/financasjpa_ex5/Despesa.java
@@ -0,0 +1,54 @@
+package br.cm.senac.financasjpa_ex5;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+
+@Entity // Havera uma tabela no banco de dados com o nome 'despesa' relacionada diretamente a essa classe
+public class Despesa {
+    
+    @Id // O atributo int id representa a chave da tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String descricao;
+    private double valor;
+    private LocalDate data;
+    
+    // Getters e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+}
--- Exemplos/FinancasJPA_Ex5/src/main/java/br/cm/senac/financasjpa_ex5/FinancasJPA_Ex5.java
+++ Exemplos/FinancasJPA_Ex5/src/main/java/br/cm/senac/financasjpa_ex5/FinancasJPA_Ex5.java
@@ -1,16 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package br.cm.senac.financasjpa_ex5;
 
-/**
- *
- * @author aline
- */
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.time.LocalDate;
+
 public class FinancasJPA_Ex5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManagerFactory fabricaEntidade = Persistence.createEntityManagerFactory("financas-pu");
+        EntityManager manager = fabricaEntidade.createEntityManager();
+    
+        // Criando o objeto a ser inserido no banco
+        Despesa gasto = new Despesa();
+        gasto.setDescricao("Primeira despesa");
+        gasto.setValor(10.50);
+        gasto.setData(LocalDate.of(2022, 5, 30));
+        
+        // Inserindo o objeto no banco de dados (CREATE)
+        manager.getTransaction().begin();
+        manager.persist(gasto);
+        manager.getTransaction().commit();
+        
+        // Recuperando dados do banco via JPA (READ)
+        Despesa gasto = manager.find(Despesa.class, 1);
+        System.out.println(gasto.getDescricao());
+        
+        // Atualizando dados do banco via JPA (UPDATE)
+        Despesa gasto = manager.find(Despesa.class, 1);
+
+        manager.getTransaction().begin();
+        gasto.setDescricao("Despesa atualizada");
+        manager.getTransaction().commit();
+
+        // Atualizando com merge() - se não for conveniente lidar com um objeto gerenciável do Entity Manager
+        Despesa gasto = new Despesa();
+        gasto.setId(1);
+        gasto.setDescricao("Atualizado com Merge");
+        gasto.setData(LocalDate.of(2022, 5, 30));
+        // Como não foi atribuido nenhum valor para a coluna 'valor', ela foi atualizada com o double padrao 0.0
+
+        manager.getTransaction().begin();
+        manager.merge(gasto);
+        manager.getTransaction().commit();
+        
+        // Deletando dados do banco (DELETE)
+        Despesa gasto = manager.find(Despesa.class, 1);
+        manager.getTransaction().begin();
+        manager.remove(gasto);
+        manager.getTransaction().commit();
+        
+        // Fechando a conexão e a fábrica
+        manager.close();
+        fabricaEntidade.close();
     }
+   
+    
 }
