fc7ddfcdec1c4f3fb2447d43b11b364eaff311cc
Aline <alineraldif@gmail.com>
2025-04-18 15:25:47 -0300

    feat: Configurando Hibernate

---
+++ Exemplos/FinancasJPA_Ex5/src/main/java/br/cm/senac/financasjpa_ex5/FinancasJPA_Ex5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.cm.senac.financasjpa_ex5;
+
+/**
+ *
+ * @author aline
+ */
+public class FinancasJPA_Ex5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
